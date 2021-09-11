f73c6c75acda0caead41eaa7e972c138606af4dd
akorolyev <83176422+akorolyev@users.noreply.github.com>
2021-09-12 00:49:23 +0300

    Create Test1.java

---
+++ src/test/java/Test1.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+public class Test1 {
+    public static void main(String args[]){
+        System.out.println("Hello world");
+    }
+
+    @Test
+    public void testJenkins(){
+        System.out.println("test_jenkins");
+    }
+
+
+
+}
